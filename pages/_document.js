3bbc3344da0621154faf459a1e74c2b8d5c9e83e
vsnikhilvs <vsnikhilvs@gmail.com>
2024-06-05 11:55:27 +0530

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { CssBaseline } from '@geist-ui/react'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { CssBaseline } from "@geist-ui/react";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    const styles = CssBaseline.flush()
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = CssBaseline.flush();
 
     return {
       ...initialProps,
@@ -14,27 +14,47 @@ class MyDocument extends Document {
           {styles}
         </>
       ),
-    }
+    };
   }
 
   render() {
     return (
       <Html>
         <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-        <link rel="manifest" href="/site.webmanifest"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="https://vsnikhilvs.github.io/personal-site/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="https://vsnikhilvs.github.io/personal-site/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="https://vsnikhilvs.github.io/personal-site/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1"
+          ></meta>
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
