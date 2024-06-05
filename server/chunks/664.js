3d651395d2bb0356d1e7c4f3a053f67d04c0b62c
vsnikhilvs <vsnikhilvs@gmail.com>
2024-06-05 11:05:23 +0530

    Updates

--- server/chunks/664.js
+++ server/chunks/664.js
@@ -18,7 +18,7 @@ Object.defineProperty(exports, "addBasePath", ({
 }));
 const _addpathprefix = __webpack_require__(751);
 const _normalizetrailingslash = __webpack_require__(387);
-const basePath =  false || "";
+const basePath = "/personal" || 0;
 function addBasePath(path, required) {
     if (false) {}
     return (0, _normalizetrailingslash.normalizePathTrailingSlash)((0, _addpathprefix.addPathPrefix)(path, basePath));
@@ -146,7 +146,7 @@ Object.defineProperty(exports, "getDomainLocale", ({
         return getDomainLocale;
     }
 }));
-const basePath = (/* unused pure expression or super */ null && ( false || ""));
+const basePath = (/* unused pure expression or super */ null && ("/personal" || 0));
 function getDomainLocale(path, locale, locales, domainLocales) {
     if (false) {} else {
         return false;
